bda6dcba7e995fa9ec26a7be2645265555d4981a
Bárbara Casac <barbaracasaclj@hotmail.com>
2022-01-17 14:34:46 -0300

    Add env file to the project

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -2,10 +2,29 @@ package database
 
 import (
 	"database/sql"
+	"github.com/spf13/viper"
+	"log"
 )
 
+func viperEnvVariable(key string) string {
+	viper.SetConfigFile(".env")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
+	value, ok := viper.Get(key).(string)
+	if !ok {
+		log.Fatalf("Invalid type assertion")
+	}
+	return value
+}
+
 func Connection() (*sql.DB, error) {
-	stringConnectionDataBase := "golang:golang@/devbook?charset=utf8&parseTime=true&loc=Local"
+	database_name := viperEnvVariable("DATABASE_NAME")
+	database_username := viperEnvVariable("DATABASE_USERNAME")
+	database_password := viperEnvVariable("DATABASE_PASSWORD")
+
+	stringConnectionDataBase := database_username + ":" + database_password + "@/" + database_name + "?charset=utf8&parseTime=true&loc=Local"
 	db, err := sql.Open("mysql", stringConnectionDataBase)
 	if err != nil {
 		return nil, err
